71f1be7bad94d68677c9336d3bc111f4d5b8b56d
Eric Bréchemier <eric@bezen.org>
2014-01-15 13:18:57 +0100

    Download spelling files for English and French during setup
    This step is not strictly necessary since user gets prompted
    to download any missing file the first time spelling is enabled;
    however, due to an error which is reported annoyingly each time
    spelling is enabled [1] when using the default English word lists
    included in Ubuntu and recent word lists for French from the download
    site [2}, I decided to download preemptively more recent files in the
    local folder ~/.vim/spell, which takes precedence over files from
    the vim distribution located in /usr/share/vim/vimcurrent/spell/.
    
    [1] Vim: Word characters differ between spell files (E763)
    http://stackoverflow.com/questions/14802136/vim-word-characters-differ-between-spell-files-e763
    
    [2] FTP archive directory /vim/runtime/spell
    http://ftp.vim.org/vim/runtime/spell/

--- setup/install.sh
+++ setup/install.sh
@@ -18,6 +18,34 @@ mkdir ~/.vim/colors
 echo 'Copy color scheme: norway today'
 cp -p .vim/colors/norwaytoday.vim ~/.vim/colors
 
+echo 'Create subdirectory for spelling files'
+mkdir ~/.vim/spell
+
+# base URL for download of binary word lists used in spelling
+spellUrl="http://ftp.vim.org/vim/runtime/spell/"
+
+echo 'Download updated files for spelling in English'
+wget \
+  --timestamping \
+  --directory-prefix=$HOME/.vim/spell \
+  "$spellUrl/README_en.txt" \
+  "$spellUrl/en.ascii.spl" \
+  "$spellUrl/en.ascii.sug" \
+  "$spellUrl/en.latin1.spl" \
+  "$spellUrl/en.latin1.sug" \
+  "$spellUrl/en.utf-8.spl" \
+  "$spellUrl/en.utf-8.sug"
+
+echo 'Download files for spelling in French'
+wget \
+  --timestamping \
+  --directory-prefix=$HOME/.vim/spell \
+  "$spellUrl/README_fr.txt" \
+  "$spellUrl/fr.latin1.spl" \
+  "$spellUrl/fr.latin1.sug" \
+  "$spellUrl/fr.utf-8.spl" \
+  "$spellUrl/fr.utf-8.sug"
+
 echo 'Create subdirectory for swap files'
 mkdir ~/.vim/swap
 
