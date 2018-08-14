8f94535d82d3d9bb44d9058016a2c5b328cab852
Eric Bréchemier <eric@bezen.org>
2018-08-14 16:21:31 +0200

    Copy whole folders, preserve an existing .vim directory
    Instead of copying files one by one into ~/.vim, whole subdirectories
    are now copied. This makes the script more resilient to changes in the
    exact list of color schemes or word lists used for spelling.

--- setup/install.sh
+++ setup/install.sh
@@ -10,40 +10,13 @@ echo "To the root of current user's home folder:"
 echo ~
 
 echo 'Create .vim folder'
-mkdir ~/.vim
+mkdir -p ~/.vim
 
-echo 'Create subdirectory for color schemes'
-mkdir ~/.vim/colors
+echo 'Copy color schemes'
+cp -R .vim/colors ~/.vim/
 
-echo 'Copy color scheme: norway today'
-cp -p .vim/colors/norwaytoday.vim ~/.vim/colors
-
-echo 'Create subdirectory for spelling files'
-mkdir ~/.vim/spell
-
-# base URL for download of binary word lists used in spelling
-spellUrl="http://ftp.vim.org/vim/runtime/spell/"
-
-getspell()
-{
-  curl -s -S "$spellUrl/$1" > "$HOME/.vim/spell/$1" 
-}
-
-echo 'Download updated files for spelling in English'
-getspell 'README_en.txt'
-getspell 'en.ascii.spl'
-getspell 'en.ascii.sug'
-getspell 'en.latin1.spl'
-getspell 'en.latin1.sug'
-getspell 'en.utf-8.spl'
-getspell 'en.utf-8.sug'
-
-echo 'Download files for spelling in French'
-getspell 'README_fr.txt'
-getspell 'fr.latin1.spl'
-getspell 'fr.latin1.sug'
-getspell 'fr.utf-8.spl'
-getspell 'fr.utf-8.sug'
+echo 'Copy word lists for spelling'
+cp -R .vim/spell ~/.vim/
 
 echo 'Create subdirectory for swap files'
 mkdir ~/.vim/swap
